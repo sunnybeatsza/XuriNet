86c75db057c2863abf5e4ebf1785a235f3d62e20
sunnybeatsza <makgerutumisho55@gmail.com>
2025-08-10 23:24:25 +0200

    added basic implementation of telegram

--- Frontend/TelegramBot/src/main/java/Bot.java
+++ Frontend/TelegramBot/src/main/java/Bot.java
@@ -1,5 +1,9 @@
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.User;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 public class Bot extends TelegramLongPollingBot {
 
@@ -10,10 +14,26 @@ public class Bot extends TelegramLongPollingBot {
     @Override
     public void onUpdateReceived(Update update) {
         // Handle messages here
+        Message message = update.getMessage();
+        User user = message.getFrom();
+
+        System.out.println(user.getFirstName() + " wrote " + message.getText() + " " + user.getId());
     }
+
 
     @Override
     public String getBotUsername() {
         return "Zuri";
+    }
+
+    public void sendText(Long who, String what){
+        SendMessage sm = SendMessage.builder()
+                .chatId(who.toString()) //Who are we sending a message to
+                .text(what).build();    //Message content
+        try {
+            execute(sm);
+        } catch (TelegramApiException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- Frontend/TelegramBot/src/main/java/Main.java
+++ Frontend/TelegramBot/src/main/java/Main.java
@@ -1,6 +1,25 @@
+import io.github.cdimascio.dotenv.Dotenv;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
 public class Main {
 
-    public static void main(String[] args) {
-        Bot zuri = new Bot();
+    public static void main(String[] args) throws TelegramApiException {
+        Dotenv dotenv = Dotenv.load();
+        String botToken = dotenv.get("TELEGRAM_TOKEN");
+        Bot zuri = new Bot(botToken);
+
+        TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
+        botsApi.registerBot(zuri);
+
+        zuri.sendText(
+                Long.valueOf(dotenv.get("USER_ID")),
+                "Hello! 👋\n\n" +
+                        "My name is Zuri, your personal safety assistant. " +
+                        "I’m here to help you stay informed, protected, and prepared.\n\n" +
+                        "If you need assistance or have any questions, just send me a message and I’ll be ready to help."
+        );
+
     }
 }
