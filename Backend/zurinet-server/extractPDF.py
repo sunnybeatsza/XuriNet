19f48a92e6ac8ebf76189ddd6adaeb7b42a560af
sunnybeatsza <makgerutumisho55@gmail.com>
2025-09-26 15:30:16 +0200

    changes to the extracting pdf code

--- Backend/zurinet-server/extractPDF.py
+++ Backend/zurinet-server/extractPDF.py
@@ -1,7 +1,15 @@
 import pdfplumber
 
-with pdfplumber.open("./zurinet-server/sampleDataSet/Gauteng_Alexandra.pdf") as pdf:
-    text = ""
-    for page in pdf.pages:
-        text += page.extract_text()
-    print(text)
+sample_pdf_file = "./zurinet-server/sampleDataSet/Gauteng_Alexandra.pdf"
+
+def extract_pdf_data(pdf_file):
+    with pdfplumber.open(pdf_file) as pdf:
+        text = ""
+        for page in pdf.pages:
+            text += page.extract_text()
+    return text
+
+
+result = extract_pdf_data(sample_pdf_file)
+print(result)
+
